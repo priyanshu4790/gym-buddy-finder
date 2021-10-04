914021269419fb7333d20cfaf06acf9a11319d88
priyanshu4790 <70906503+priyanshu4790@users.noreply.github.com>
2021-10-04 23:49:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -54,5 +54,5 @@ const server = http.createServer(app);
 
 setupSockets(server, memoryStore);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000; //later update 5000
 server.listen(PORT, () => console.log('Server running on port ' + PORT));
